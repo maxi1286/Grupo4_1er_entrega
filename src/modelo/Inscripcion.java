b79b1aa924c18419469b09c550c2a9db1ddc548f
AbreguJuan <abregu058@gmail.com>
2024-10-01 17:11:48 -0300

    Clase Materia y Inscripcion
    A ver si se actualiza esta melda!!

--- src/modelo/Inscripcion.java
+++ src/modelo/Inscripcion.java
@@ -11,4 +11,62 @@ package modelo;
  */
 public class Inscripcion {
     
+    public int idInscripto;
+    public int nota;
+    public int idAlumno;
+    public int idMateria;
+
+    public Inscripcion() {
+    }
+
+    public Inscripcion(int nota, int idAlumno, int idMateria) {
+        this.nota = nota;
+        this.idAlumno = idAlumno;
+        this.idMateria = idMateria;
+    }
+
+    public Inscripcion(int idInscripto, int nota, int idAlumno, int idMateria) {
+        this.idInscripto = idInscripto;
+        this.nota = nota;
+        this.idAlumno = idAlumno;
+        this.idMateria = idMateria;
+    }
+
+    public int getIdInscripto() {
+        return idInscripto;
+    }
+
+    public int getNota() {
+        return nota;
+    }
+
+    public int getIdAlumno() {
+        return idAlumno;
+    }
+
+    public int getIdMateria() {
+        return idMateria;
+    }
+
+    public void setIdInscripto(int idInscripto) {
+        this.idInscripto = idInscripto;
+    }
+
+    public void setNota(int nota) {
+        this.nota = nota;
+    }
+
+    public void setIdAlumno(int idAlumno) {
+        this.idAlumno = idAlumno;
+    }
+
+    public void setIdMateria(int idMateria) {
+        this.idMateria = idMateria;
+    }
+
+    @Override
+    public String toString() {
+        return "idInscripto: " + idInscripto + ", nota: " + nota + ", idAlumno: " + idAlumno + ", idMateria: " + idMateria;
+    }
+    
 }
--- src/modelo/Materia.java
+++ src/modelo/Materia.java
@@ -10,5 +10,62 @@ package modelo;
  * @author Nedisane
  */
 public class Materia {
+    public int IdMateria;
+    public String Nombre;
+    public int Anio;
+    public boolean Estado;
+
+    public Materia() {
+    }
+
+    public Materia(String Nombre, int Anio, boolean Estado) {
+        this.Nombre = Nombre;
+        this.Anio = Anio;
+        this.Estado = Estado;
+    }
+
+    public Materia(int IdMateria, String Nombre, int Anio, boolean Estado) {
+        this.IdMateria = IdMateria;
+        this.Nombre = Nombre;
+        this.Anio = Anio;
+        this.Estado = Estado;
+    }
+
+    public int getIdMateria() {
+        return IdMateria;
+    }
+
+    public void setIdMateria(int IdMateria) {
+        this.IdMateria = IdMateria;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public int getAnio() {
+        return Anio;
+    }
+
+    public void setAnio(int Anio) {
+        this.Anio = Anio;
+    }
+
+    public boolean isEstado() {
+        return Estado;
+    }
+
+    public void setEstado(boolean Estado) {
+        this.Estado = Estado;
+    } 
     
+    @Override
+    public String toString() {
+        return IdMateria+" "+Nombre+" Año: "+Anio;
+    }
+      
 }
